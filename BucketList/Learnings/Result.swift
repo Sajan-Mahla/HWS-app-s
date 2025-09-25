f1b571e304757a256f052ef0cbb960c6d2177b2b
Sajan Mahla <sajanmahla965@gmail.com>
2025-09-25 12:08:26 +0530

    Add files via upload

---
+++ BucketList/Learnings/Result.swift
@@ -0,0 +1,22 @@
+//
+//  Result.swift
+//  Bucket List
+//
+//  Created by SAJAN  on 24/09/25.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
